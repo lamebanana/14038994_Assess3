25d56f286b9a01a53d898895c658af3e38939848
lamebanana <f.ginevra98@gmail.com>
2022-09-06 17:55:53 +1000

    added level layout

--- Assets/Scripts/LevelLayout.cs
+++ Assets/Scripts/LevelLayout.cs
@@ -10,6 +10,8 @@ public class LevelLayout : MonoBehaviour
     public Sprite emptyTile, outsideCorner, outsideWall,
                         insideCorner, insideWall, pellet, powerPellet, junction;
 
+
+    public string[] leveltype = {"Full", "Bottom quarter", "Half bottom", "Half upper"};
     public bool update = false;
     public bool set_position = false;
     private int[,] levelMap = 
@@ -31,18 +33,29 @@ public class LevelLayout : MonoBehaviour
         {0,0,0,0,0,0,5,0,0,0,4,0,0,0}, 
     }; 
 
-    private GameObject[,] tiles;
+    private int[,] levelMapHFlip, levelMapVFlip;
+    private int[,] completeLevel = new int[30, 28];
+    private int[,] halfLevelBottom = new int[15,28];
+    private int[,] halfLevelUp = new int[15,28];
+
+    private int[,] rotationMapHFlip, rotationMapVFlip;
+    private int[,] completeRotation = new int[30, 28];
+    private int[,] halfRotationBottom = new int[15,28];
+    private int[,] halfRotationUp = new int[15,28];
+
 
 // these rotations are calculated for croner sprites like |^ and centered walls like |
     private int [,] rotationMap =
     {
         {90,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
         {90,0,0,0,0,0,0,0,0,0,0,0,0,90}, 
-        {90,0,90,0,0,0,0,90,0,0,0,0,0,90}, // ricontrolla questa riga
+        {90,0,90,0,0,0,0,90,0,0,0,0,0,90},
         {90,0,90,0,0,90,0,90,0,0,0,90,0,90}, 
         {90,0,180,0,0,-90,0,180,0,0,0,-90,0,180}, 
         {90,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
-        {90,0,90,0,0,0,0,0,90,0,90,0,0,0}, 
+
+
+        {90,0,90,0,0,0,0,90,0,0,90,0,0,0,}, 
         {90,0,180,0,0,-90,0,90,90,0,180,0,0,0}, 
         {90,0,0,0,0,0,0,90,90,0,0,0,0,90}, 
         {180,0,0,0,0,0,0,90,180,0,0,0,0,90}, 
@@ -71,24 +84,59 @@ public class LevelLayout : MonoBehaviour
         0,0,0,0,0,0,0,0,0,0,90,0,0,0 
     };
 
+    private int[,] rotation, level;
+
     void Awake(){
-        for (int i = 0; i < levelMap.GetLength(0); i++){
-                for (int j = 0; j < levelMap.GetLength(1); j++){ 
-                    DrawTile(i,j, levelMap[i,j]);  
+
+
+        level = levelMap;
+        rotation = rotationMap;
+
+        for (int i = 0; i < halfLevelBottom.GetLength(0); i++){
+                for (int j = 0; j < halfLevelBottom.GetLength(1); j++){ 
+                    DrawTile(i,j, halfLevelBottom[i,j]);  
                 }
             }
     }
     // Update is called once per frame
     void Update()
     {
+
+        if (leveltype.Equals("Full")){
+
+            level = completeLevel;
+            rotation = completeRotation;
+
+        }else if (leveltype.Equals("Half bottom")){
+            level = halfLevelBottom;
+            rotation = halfRotationBottom;
+        }else if (leveltype.Equals("Half upper")){
+            level = halfLevelUp;
+            rotation = halfRotationUp;
+        }else {
+            level = levelMap;
+            rotation = rotationMap;
+        }
+
         if (update){
 
             DestroyAllTiles();
             update = false;
 
-            for (int i = 0; i < levelMap.GetLength(0); i++){
-                for (int j = 0; j < levelMap.GetLength(1); j++){
-                    DrawTile(i,j, levelMap[i,j]);
+            //int c = 0;
+            for (int i = 0; i < 30; i++){
+                for (int j = 0; j < 28; j++){
+
+                    FlipHorizontallyLevel();
+                    CreateBelowPart();
+
+                    FlipVerticallyLevel();
+                    
+                    if(i < 15)
+                        DrawTile(i,j, halfLevelBottom[i,j]);
+                    else {
+                        DrawTile(i,j, halfLevelUp[i - 15,j]);
+                    }
                 }
             }
         }
@@ -106,10 +154,32 @@ public class LevelLayout : MonoBehaviour
         //rotationMap2[92] = 0;
         GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tiles");
 
-        for(int n = 0; n < tiles.Length; n++)
-        {
-            tiles[n].transform.eulerAngles = new Vector3(0,0,rotationMap2[n]);
-        }
+        int n = 0;
+        for (int i = 0; i < 30; i ++){
+            for (int j = 0; j < 28; j++){
+
+                // j è la y
+                // i è la x
+
+                if (j > 13 && i <15){ //quadrante in alto a sx
+                    /*if (completeLevel[i,j] == 3 || completeLevel[i,j] == 1){
+                        tiles[n].transform.eulerAngles = new Vector3(180,0, halfRotationBottom[i,j]);
+                    } else */tiles[n].transform.eulerAngles = new Vector3(180,0,halfRotationBottom[i,j]);
+                } else if (i < 15 && j <=14){ // quadrante in basso a sx
+                    tiles[n].transform.eulerAngles = new Vector3(0,0,halfRotationBottom[i,j]);
+                } else if (i >= 15){ // quadrante destro
+                    tiles[n].transform.eulerAngles = new Vector3(180,180,halfRotationUp[i - 15,j]);
+                    if (j <14){
+                        tiles[n].transform.eulerAngles = new Vector3(0,180,halfRotationUp[i - 15,j]);
+                    }
+                } 
+                    
+                
+                n++;
+
+                //if (n > 410) break;
+            }
+        } 
             
     }
 
@@ -123,7 +193,76 @@ public class LevelLayout : MonoBehaviour
         }
     }
 
+
+    void CreateBelowPart(){
+
+        for (int i = 0; i < 15; i++){
+            for (int j = 0; j < 28; j++){
+                if(j < 14){
+                    halfLevelBottom[i,j] = levelMap[i,j];
+                    halfRotationBottom[i,j] = rotationMap[i,j];
+                } else{
+                    halfLevelBottom[i,j] = levelMapHFlip[i, j-14];
+                    halfRotationBottom[i,j] = rotationMapHFlip[i, j -14];
+                }
+            }
+        }
+    }
+
     
+
+    void FlipVerticallyLevel()
+    {
+        halfLevelUp = new int[15,28];
+        halfRotationUp = new int[15,28];
+
+        int columns = halfLevelBottom.GetLength(1);
+        int rows = halfLevelBottom.GetLength(0);
+
+        int i = 0;
+  
+        //fliph
+        for (int column = 0; column < columns; column++)
+        {
+            i = 0;
+
+            //flip row per each column
+            for (int index = rows-1; index >= 0; index--)
+            {
+                halfLevelUp[i, column] = halfLevelBottom[index,column];
+                halfRotationUp[i,column] = halfRotationBottom[index,column];
+                i++;
+            } 
+        }
+
+        
+    }
+
+    void FlipHorizontallyLevel()
+    {
+        levelMapHFlip = new int[15,14];
+        rotationMapHFlip = new int[15,14];
+        int columns = levelMap.GetLength(1);
+        int rows = levelMap.GetLength(0);
+        int i = 0;
+        
+        //fliph
+        for (int row = 0; row < rows; row++)
+        {
+            i = 0;
+
+            //flip row per each column
+            for (int index = columns-1; index >= 0; index--)
+            {
+                levelMapHFlip[row, i] = levelMap[row,index];
+                rotationMapHFlip[row,i] = rotationMap[row,index];
+                i++;
+            } 
+        }
+
+        //levelMap = levelMapHFlip;
+    }
+
 
     private void DrawTile(int x, int y, int val){
         GameObject g = new GameObject();
