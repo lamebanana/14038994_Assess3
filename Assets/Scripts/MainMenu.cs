ccc1b743ecc5f15d51277bc1dd6d9d0720c3ec82
lamebanana <f.ginevra98@gmail.com>
2022-10-05 18:36:16 +1100

    pass 50 finished

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    public void PlayLevel1(){
+
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
