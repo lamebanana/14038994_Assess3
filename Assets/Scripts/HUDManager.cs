14248041e5cee75e0b14199f6824b9aaf145176e
lamebanana <f.ginevra98@gmail.com>
2022-10-05 22:14:28 +1100

    finished pass60

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    public void ExitLevel(){
+
+        SceneManager.LoadScene("StartScene");
+    }
+
+}
